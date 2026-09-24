0fdbf9097dcd2ab420fdff6166c9b4b2c5973b5b
Silas Dermer <135365196+silasmhd@users.noreply.github.com>
2026-09-24 13:26:28 -0700

    Cleanup and comments

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -37,23 +37,6 @@
      })
  )
  
-//  const server = setupServer(
-//     http.get("https://romans.justyy.workers.dev/api/romans/", ({ request }) => {
-        // const url = new URL(request.url);
-        // const query = url.searchParams.get("n")
-        // if (query == "166") {
-        //    console.log("166 called")                                            
-        //     return HttpResponse.json({
-        //         "error": null,
-        //         "input": "166",
-        //         "result": "CLXVI"
-        //     })
-        // } else {
-        //     return HttpResponse.json(modernNumeralsResults)
-        // }
-//     })
-// )
-
  beforeAll(function () {
      server.listen()
  })
@@ -66,8 +49,12 @@
      server.close();
  })
 
+
+/*
+* Tests below
+*/
  
- test("old roman numeral dispalys the correct result for a single number", async function () {
+ test("Old roman numeral dispalys the correct result for a single number", async function () {
     initDomFromFiles(
         __dirname + "/romanNumerals.html",
         __dirname + "/romanNumerals.js"
@@ -81,7 +68,7 @@
     expect(oldResult).toHaveTextContent("V")
 })
 
-test("old roman numeral dispalys the correct result for a large number", async function () {
+test("Old roman numeral dispalys the correct result for a large number", async function () {
     initDomFromFiles(
         __dirname + "/romanNumerals.html",
         __dirname + "/romanNumerals.js"
@@ -93,9 +80,7 @@ test("old roman numeral dispalys the correct result for a large number", async f
     expect(oldResult).toHaveTextContent("CLXVI")
 })
 
-
-
-test("input 166 shows CXXIII in modern", async function () {
+test("Input 166 shows CLXVI in modern", async function () {
     initDomFromFiles(
       __dirname + "/romanNumerals.html",
       __dirname + "/romanNumerals.js"
@@ -115,28 +100,7 @@ test("input 166 shows CXXIII in modern", async function () {
     })
   })
 
-  test("input 166 and generates modern, then adds new input to clear modern", async function () {
-    initDomFromFiles(
-      __dirname + "/romanNumerals.html",
-      __dirname + "/romanNumerals.js"
-    )
-    
-    const user = userEvent.setup()
-    const button = domTesting.getByRole(document, "button")
-    const input = domTesting.getByLabelText(document, "Arabic number (1-3999)")
-    
-    await user.type(input, "166")
-    await user.click(button)
-    await user.type(input, "166")
-
-    const newResult = document.getElementById("modern-roman-result");
-
-    await domTesting.waitFor(() =>  {
-        expect(newResult).toHaveTextContent("")
-    })
-  })
-  
-  test("input 166 and generates modern, then adds new input to clear modern", async function () {
+  test("Input 166 and generates modern, then adds new input to clear modern", async function () {
     initDomFromFiles(
       __dirname + "/romanNumerals.html",
       __dirname + "/romanNumerals.js"
